15120b6569c8a8dc02cb29ebe9578283d88d9f32
Nikolay Danailov <frostblooded@yahoo.com>
2016-06-06 12:36:20 +0300

    Make main controller use new communication protocol

--- MainControllerBatteries.ino
+++ MainControllerBatteries.ino
@@ -6,6 +6,7 @@
 #define GPRS_APN ""
 #define GPRS_LOGIN ""
 #define GPRS_PASSWORD ""
+#define PACKET_SIZE 1+2*sizeof(float)+2
 
 GSMClient client;
 GPRS gprs;
@@ -28,13 +29,46 @@ void setup()
   Serial.begin(1200);
 }
 
+// Makes url-encoded string to send to server
+String makeString(int index, float voltage, float current) {
+  String res;
+  
+  res += "battery_id=" + String(index);
+  res += "&voltage=" + String(voltage);
+  res += "&current=" + String(current);
+  
+  return res;
+}
+
 String getDeviceData(int index) {
   Serial.print(String(index));
 
   while (!Serial.available())
     delay(50);
 
-  return Serial.readString();
+  String data = Serial.readString();
+  const char* str = data.c_str();
+  if(data.length() != PACKET_SIZE) {
+    // Error
+  } else {
+    int index_rcv = str[0];
+    if(index != index_rcv) {
+      // Error... again
+    } else {
+      char cs = 0;
+      for(int i=0; i<PACKET_SIZE-1; i++) {
+        cs += str[i];
+      }
+      if(cs != str[PACKET_SIZE-1]) {
+        //AGAIN ERROR!!!
+      } else {
+        float voltage, current;
+        voltage = *((float*)&str[1]);
+        current = *((float*)&str[1+sizeof(float)]);
+        return makeString(index, voltage, current);
+      }
+    }
+  }
 }
 
 void wakeUpNow()
